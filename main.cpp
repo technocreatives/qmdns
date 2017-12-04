aad6a2ec87f2d7f408881ee40b664471f443799e
Emiliano Firmino <emiliano@technocreatives.com>
2017-12-04 16:26:44 +0100

    Connect slots to test callbacks
    Signed-off-by: Emiliano Firmino <emiliano@technocreatives.com>

--- main.cpp
+++ main.cpp
@@ -33,8 +33,29 @@ void stop() {
     QCoreApplication::quit();
 }
 
+void svc_found(QmDNSService* svc) {
+    qDebug() << "ServiceFound: " << qUtf8Printable(svc->getName());
+}
+
+void svc_lost(QmDNSService* svc) {
+    qDebug() << "ServiceLost: " << qUtf8Printable(svc->getName());
+}
+
+void svc_resolved(QmDNSService* svc) {
+    qDebug() << "ServiceResolved: " << qUtf8Printable(svc->getName()) << qUtf8Printable(svc->getHostname()) << qUtf8Printable(svc->getIPv4Address()) << svc->getPort();
+}
+
+void svc_not_resolved(QmDNSService* svc) {
+    qDebug() << "ServiceNotResolved: " << qUtf8Printable(svc->getName());
+}
+
 int main(int argc, char *argv[])
 {
+    QObject::connect(&mdns, &QmDNS::serviceFound, svc_found);
+    QObject::connect(&mdns, &QmDNS::serviceLost, svc_lost);
+    QObject::connect(&mdns, &QmDNS::serviceResolved, svc_resolved);
+    QObject::connect(&mdns, &QmDNS::serviceNotResolved, svc_not_resolved);
+
     QCoreApplication a(argc, argv);
     QTimer::singleShot(0, &start);
     QTimer::singleShot(60* 1000, &stop);
